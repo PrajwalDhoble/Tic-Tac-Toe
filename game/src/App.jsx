4763abb25eb819f0f5f3be0f0ddc22926f59ffaa
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-05-13 14:52:46 +0530

    all done

---
+++ game/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './component/tic_tac_toe/tic_tac_toe';
+
+function App() {
+  return (
+    <div className="App">
+        <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
